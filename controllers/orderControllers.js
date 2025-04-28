4dff742d84cb85fc85148ba77f291a54b6d85a48
tijina123 <tijinamariamtitus@gmail.com>
2025-04-28 18:30:29 +0530

    razorpay backend

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -539,6 +539,23 @@ const shiprocketWebhook = async (req, res) => {
   }
 };
 
+// Payment Verification
+const verifyPayment = (req, res) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+    .update(body)
+    .digest("hex");
+
+  if (expectedSignature === razorpay_signature) {
+    res.json({ success: true });
+  } else {
+    res.status(400).json({ success: false, message: "Invalid signature sent!" });
+  }
+};
+
 module.exports = {
   createOrder,
   shiprocketWebhook,
@@ -548,4 +565,5 @@ module.exports = {
   getAllOrdersByUser,
   orderForGraph,
   salesByCategory,
+  verifyPayment
 };
